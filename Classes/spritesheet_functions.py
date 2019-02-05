39d6127d838db2297c5992efd789d4d2acc5fe4f
arthur-du <arthurduca38@gmail.com>
2019-02-05 13:50:32 +0100

    ajout de la gestion des sprite

--- Classes/spritesheet_functions.py
+++ Classes/spritesheet_functions.py
@@ -6,4 +6,17 @@ import pygame
 import constants
 
 class SpriteSheet(object):
-    
+    # Grab images out of a sprite
+    sprite_sheet = None
+
+    def __init__(self, file_name):
+        self.sprite_sheet = pygame.image.load(file_name).convert()
+
+    def get_image(self, x, y, width, height):
+        # Create a new blank image
+        image = pygame.Surface([width, height]).convert()
+        # Copy the sprite from the large sheet onto the smaller image
+        image.blit(self.sprite_sheet, (0, 0), (x, y, width, height))
+        # Black works as the transparent color, can change
+        image.set_colorkey(constants.BLACK)
+        return image
